efc29e6e2ce044cc18c0cbf991ad4af5d24d307e
lzieberm <laura.ziebermayr@gmail.com>
2026-03-13 08:12:13 +0100

    Refill the Interaction-model

--- impl/model/interaction.py
+++ impl/model/interaction.py
@@ -1,8 +1,10 @@
 class Interaction:
 
-    def __init__(self, response, res_id = ""):
-        self.response_id = res_id   
-        self.response = response
+    def __init__(self,direction , header, interaction, res_id = ""):
+        self.direction = direction      # response or request (could also rename to type)
+        self.header = header            # header of interaction
+        self.interaction = interaction  # Body of interaction
+        self.res_id = res_id
 
     def __repr__(self):
-        return f"Response( id = {self.response_id}, {self.response.text})"
+        return f"Interaction {self.direction} = ( id = {self.res_id}, {self.interaction})"
