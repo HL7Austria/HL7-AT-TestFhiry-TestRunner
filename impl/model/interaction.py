92476e94755bbcc196484808ffecf02465dd7919
lzieberm <laura.ziebermayr@gmail.com>
2026-03-13 15:27:35 +0100

    support a very basic saving of a response

--- impl/model/interaction.py
+++ impl/model/interaction.py
@@ -1,10 +1,11 @@
 class Interaction:
 
-    def __init__(self,direction , header, interaction, res_id = ""):
+    def __init__(self,direction , header, interaction,status_code = "", res_id = ""):
         self.direction = direction      # response or request (could also rename to type)
         self.header = header            # header of interaction
         self.interaction = interaction  # Body of interaction
+        self.status_code = status_code  # just trying new things
         self.res_id = res_id
 
     def __repr__(self):
-        return f"Interaction {self.direction} = ( id = {self.res_id}, {self.interaction})"
+        return f"Interaction {self.direction} = ( id = {self.res_id}, header = {self.header}, body = {self.interaction})"
