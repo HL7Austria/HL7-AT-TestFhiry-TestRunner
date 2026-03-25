6e43d656c572934b3aad82a7ec3f023e539f642d
lzieberm <laura.ziebermayr@gmail.com>
2026-03-25 10:16:02 +0100

    fix saving issues and replace variables inside operations

--- impl/test_script_evaluator/test_script_evaluator_log_to_file.py
+++ impl/test_script_evaluator/test_script_evaluator_log_to_file.py
@@ -61,7 +61,6 @@ def execute_operation(operation):
     json_str = json.dumps(operation)
     result = pattern.sub(replacer, json_str)
     operation = json.loads(result)
-    print(operation)
 
 
     #get all Info from operation
@@ -77,6 +76,7 @@ def execute_operation(operation):
         "Content-Type": parse_fhir_header(operation.get("contentType")),
         "Accept": parse_fhir_header(operation.get("accept")),
     }
+   
    
 
     if type == "create":
