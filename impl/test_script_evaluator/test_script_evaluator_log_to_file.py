4efef2d52e83098e0e3d5cf34ef7d8dc39fda6cb
lzieberm <laura.ziebermayr@gmail.com>
2026-06-02 08:30:49 +0200

    remove unnecessary main function

--- impl/test_script_evaluator/test_script_evaluator_log_to_file.py
+++ impl/test_script_evaluator/test_script_evaluator_log_to_file.py
@@ -894,14 +894,3 @@ def test_fhir_operations(testscript_data):
     REQ_RESP.clear()
     PROFILES.clear()
     VARIABLES.clear()
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="FHIR TestScript Runner")
-    parser.add_argument("--config", required=True, help="Path to config.json")
-    args = parser.parse_args()
-
-    conf_man.get_config_manager(args.config)
-
-    for testscript_path, resource_path in conf_man.get_testscript_pairs():
-        testscript_data = load_testscript_data(testscript_path, resource_path)
-        test_fhir_operations(testscript_data)
